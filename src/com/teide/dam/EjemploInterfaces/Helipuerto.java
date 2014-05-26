5d1cc8150ce01eab444761d2942d891947552868
DAM1 <DAM1@PC800>
2014-05-26 10:07:52 +0200

    cambios

--- src/com/teide/dam/EjemploInterfaces/Helipuerto.java
+++ src/com/teide/dam/EjemploInterfaces/Helipuerto.java
@@ -10,25 +10,12 @@ import java.util.ArrayList;
  *
  * @author DAM1
  */
-public class Helipuerto {
-    
-    ArrayList<Volador>listado = new ArrayList<>();
-    
-    public void añadir (Volador v) {
-        listado.add(v);
-    }
-    
-    public boolean eliminar (int eliminacion) {
-        listado.remove(eliminacion);
-        return true;
-    }
-    
+public class Helipuerto {    
+    ArrayList<Volador>listado = new ArrayList<>();    
     String aterrizar(int numero, int metros){
        return listado.get(numero).aterrizar(metros);
     }
     String despegar(int numero){
        return listado.get(numero).despegar();
-    }
-      //sdfsfsdfsdf eeeeeeeeeeeeee    
-     
+    }     
 }
